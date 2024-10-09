1b48fe331675e73eb83cdf711df4eead117f830b
Juan Jose Cano <juanjocm222003@gmail.com>
2024-10-09 09:57:37 -0600

    Mejore el responsive

--- mytinerary-setup-initial/src/Components/Footer.jsx
+++ mytinerary-setup-initial/src/Components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer(params) {
 <footer className="w-full  p-4 bg-gradient-to-r from-gray-900 to-gray-800 shadow md:px-6 md:py-8 dark:bg-gray-800">
             <div className="max-w-6xl mx-auto flex flex-col sm:flex-row sm:items-center sm:justify-between">
                 <a href="#" target="_blank" rel="noopener noreferrer" className="flex items-center mb-4 sm:mb-0">
-                    <img src="https://www.expedia.mx/_dms/header/logo.svg?locale=es_MX&siteid=12&2&6f9ec7db" className="mr-4 h-6" alt="Logo" />
+                    <img src="https://logos-world.net/wp-content/uploads/2021/05/Expedia-Symbol.png" className="mr-4 h-6" alt="Logo" />
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">MY TINERARY</span>
                 </a>
                 <ul className="flex flex-wrap items-center justify-center mb-6 sm:mb-0">
